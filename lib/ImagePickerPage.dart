76472db442f4a24f305a321a3bfb2a57f08c5aa9
vignesh <vignesh@systimanx.com>
2020-02-21 13:40:53 +0530

    1.Map and image picker function implement

---
+++ lib/ImagePickerPage.dart
@@ -0,0 +1,40 @@
+import 'dart:io';
+
+import 'package:image_picker/image_picker.dart';
+import 'package:flutter/material.dart';
+
+class ImagePickerPage extends StatefulWidget {
+  @override
+  _ImagePickerPageState createState() => _ImagePickerPageState();
+}
+
+class _ImagePickerPageState extends State<ImagePickerPage> {
+  File _image;
+
+  Future getImage() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      _image = image;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Image Picker Example'),
+      ),
+      body: Center(
+        child: _image == null
+            ? Text('No image selected.')
+            : Image.file(_image),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: getImage,
+        tooltip: 'Pick Image',
+        child: Icon(Icons.add_a_photo),
+      ),
+    );
+  }
+}
